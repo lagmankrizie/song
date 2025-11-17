336447c1ec2ad68831078dee30f8087bd0b6a739
lagmankrizie <lagmankrizie@gmail.com>
2025-11-17 16:15:50 +0800

    Update song.php

--- song.php
+++ song.php
@@ -48,7 +48,7 @@ $verbs = ["could","freeze","make","want","know","tell","mean","kiss",
 $verbIs ='is';
 $verse1 = ["If I ",", Id " ," this " ," it my ",
 "Youre all I " ," to want to ",
-"I can",", you" ,"it when you" ,"me slow",
+"I can ",", you" ," it when you " ," me slow ",
 "But please," ,"me," ,"the" ,"no",
 "In a perfect",", Id" ,"to", "you the loudest",
 "But all I" ,"is" ,"to", "you soundless",
@@ -57,13 +57,15 @@ $verse1 = ["If I ",", Id " ," this " ," it my ",
 ?>
 <!DOCTYPE html>
 <html>
-    <head></head>
+    <head>
+        <link rel="stylesheet" href="styles.css">
+    </head>
     <body>
         <h1>Anaheim</h1>
         <h2>by <?php echo $artist;?></h2>
         <h3>[Verse 1]</h3>
         <p><?php echo $verse1[0].$verbs[0].$verse1[1].$verbs[1].$verse1[2].$nouns[0].$verbs[2].$verse1[3].$nouns[1]?></p>
         <p><?php echo $verse1[4].$verbs[3].$verse1[5].$verbs[4]?></p>
-        <p><?php echo $verse1[6].$verbs[5].$verse1[7].$verbs[6]. $verse[8]?></p>
+        <p><?php echo $verse1[6].$verbs[5].$verse1[7].$verbs[6].$verse1[8].$verbs[7].$verse1[9]?></p>
     </body>
 </html>
